446de6fc0d381a04f444a5d55c5451f3cfd13cc3
Rob Clark <rob@ti.com>
2012-01-21 13:47:39 -0600

    Only enable v4l2/dmabuf tests if supported
    Test v4l2 headers, and only enable v4l2/dmabuf tests if dmabuf support
    is detected.

---
+++ cam-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# link pipeline:
+/usr/bin/media-ctl -d /dev/media0 -r -l '"OMAP4 ISS CSI2a":1 -> "OMAP4 ISS CSI2a output":0 [1]'
+
+# set formats:
+/usr/bin/media-ctl -d /dev/media0 -r -f '"ov5640 3-003c":0 [UYVY 640x480]','"OMAP4 ISS CSI2a":0 [UYVY 640x480]'
+
+# print setup:
+/usr/bin/media-ctl -d /dev/media0 -p
+
